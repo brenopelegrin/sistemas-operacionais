2a85305c1fc7633d1df4ab1822d453662849675f
Breno Pelegrin <breno.pelegrin@usp.br>
2025-03-31 18:23:46 -0300

    ADD: deploy.sh script for VM

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Starting deploy of the repository..."
+echo "PLEASE MAKE SURE TO SETUP SSH KEYS PREVIOUSLY!"
+printf "\n"
+echo "[pre-deploy] showing versions of docker and git..."
+git --version
+docker --version
+printf "\n"
+echo "[1] Removind old data."
+rm -rf ~/projeto-ssc0541-grupo-06
+printf "\n"
+echo "[2] Cloning repository into '~/projeto-ssc0541-grupo-06'."
+printf "\n"
+git clone git@github.com:ICMC-SSC0541-2025/projeto-ssc0541-grupo-06.git ~/projeto-ssc0541-grupo-06
+printf "\n"
+echo "[3] Changing branch to main and getting latest commits."
+printf "\n"
+cd projeto-ssc0541-grupo-06 && git fetch --all && git checkout main && git reset --hard checkpoint1-submission
+printf "\n"
+echo "[DONE] Repository cloned and set up."
+echo "The repository is available at '~/projeto-ssc0541-grupo-06'."
+echo "The instructions to run the project are located at '~/projeto-ssc0541-grupo-06/README.md'."
+echo "PLEASE READ the instructions to understand the architecture and how to run."
