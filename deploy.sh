83b2d9ca19450fdf6ce2bc3ce23c3b6f9982aacd
Breno Pelegrin <breno.pelegrin@usp.br>
2025-05-06 16:57:48 -0300

    FIX: point the deploy script to checkpoint2

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,12 @@ echo "[2] Cloning repository into '~/projeto-ssc0541-grupo-06'."
 printf "\n"
 git clone git@github.com:ICMC-SSC0541-2025/projeto-ssc0541-grupo-06.git ~/projeto-ssc0541-grupo-06
 printf "\n"
-echo "[3] Changing branch to main and getting latest commits."
+echo "[3] Resetting HEAD to the tag checkpoint2-submission."
 printf "\n"
-cd projeto-ssc0541-grupo-06 && git fetch --all && git checkout main && git reset --hard checkpoint1-submission
+cd projeto-ssc0541-grupo-06 && git fetch --all && git checkout main && git reset --hard checkpoint2-submission
 printf "\n"
+echo "[4] Removing .git to make the repository static."
+rm -rf ~/projeto-ssc0541-grupo-06/.git
 echo "[DONE] Repository cloned and set up."
 echo "The repository is available at '~/projeto-ssc0541-grupo-06'."
 echo "The instructions to run the project are located at '~/projeto-ssc0541-grupo-06/README.md'."
