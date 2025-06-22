722cdaac1a4c795dad3b53aa407595a0a3c54f48
Breno Pelegrin <breno.pelegrin@usp.br>
2025-06-22 19:27:56 -0300

    FIX: change checkpoint1 to redirect strace, time stats to stdout

--- checkpoint1/entrypoint.sh
+++ checkpoint1/entrypoint.sh
@@ -5,42 +5,42 @@ printf "\n";
 
 # cpu-bound
 echo "Running 'strace' for 'cpu-bound':";
-strace -c /usr/app/checkpoint1/bin/cpu-bound;
+strace -c /usr/app/checkpoint1/bin/cpu-bound 2>&1;
 printf "\n";
 echo "Running 'time' for 'cpu-bound':";
-/usr/bin/time -v /usr/app/checkpoint1/bin/cpu-bound;
+{ /usr/bin/time -v /usr/app/checkpoint1/bin/cpu-bound; } 2>&1;
 printf "\n";
 
 # io-bound
 echo "Running 'strace' for 'io-bound':";
-strace -c /usr/app/checkpoint1/bin/io-bound;
+strace -c /usr/app/checkpoint1/bin/io-bound 2>&1;
 printf "\n";
 echo "Running 'time' for 'io-bound':";
-/usr/bin/time -v /usr/app/checkpoint1/bin/io-bound;
+{ /usr/bin/time -v /usr/app/checkpoint1/bin/io-bound; } 2>&1;
 printf "\n";
 
 # memory
 echo "Running 'strace' for 'memory':";
-strace -c /usr/app/checkpoint1/bin/memory;
+strace -c /usr/app/checkpoint1/bin/memory 2>&1;
 printf "\n";
 echo "Running 'time' for 'memory':";
-/usr/bin/time -v /usr/app/checkpoint1/bin/memory;
+{ /usr/bin/time -v /usr/app/checkpoint1/bin/memory; } 2>&1;
 printf "\n";
 
 # processes
 echo "Running 'strace' for 'processes':";
-strace -c /usr/app/checkpoint1/bin/processes;
+strace -c /usr/app/checkpoint1/bin/processes 2>&1;
 printf "\n";
 echo "Running 'time' for 'processes':";
-/usr/bin/time -v /usr/app/checkpoint1/bin/processes;
+{ /usr/bin/time -v /usr/app/checkpoint1/bin/processes; } 2>&1;
 printf "\n";
 
 # io-and-files
 echo "Running 'strace' for 'io-and-files':";
-strace -c /usr/app/checkpoint1/bin/io-and-files;
+strace -c /usr/app/checkpoint1/bin/io-and-files 2>&1;
 printf "\n";
 echo "Running 'time' for 'io-and-files':";
-/usr/bin/time -v /usr/app/checkpoint1/bin/io-and-files;
+{ /usr/bin/time -v /usr/app/checkpoint1/bin/io-and-files; } 2>&1;
 printf "\n";
 
 echo "Finished."
